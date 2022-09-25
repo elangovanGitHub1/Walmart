e23e7699ea3b4cfe105ddaee37a8bd15a356ce51
elangovan0 <112743516+elangovan0@users.noreply.github.com>
2022-09-25 22:00:49 +0530

    Update w-mul.sh

--- w-mul.sh
+++ w-mul.sh
@@ -1,6 +1,5 @@
 echo ""
 echo "shell program to mul 2 values"
-echo "this program developed by vinayagam and Arun"
 echo ""
 read -p "enter the value of a is: " a
 read -p "enter the value of b is: " b
