41aff932de1e08dd034493d9bce037629994fc02
elangovan0 <112743516+elangovan0@users.noreply.github.com>
2022-09-25 18:59:39 +0530

    Update w-add.sh

--- w-add.sh
+++ w-add.sh
@@ -1,7 +1,5 @@
 echo ""
 echo "shell program to add 2 values"
-echo "shell walmart project"
-echo "this shell program was recently updated by arun in remote repo"
 echo ""
 read -p "enter the value of a is: " a
 read -p "enter the value of b is: " b
